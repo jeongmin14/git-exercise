3b72d86c4373ce0bc21e1fa57e826865cc27d5d5
konhee-kim <kghee218@gmail.com>
2021-01-26 13:38:39 +0900

    Fix : test.py fixed - hi universe

--- test.py
+++ test.py
@@ -6,28 +6,27 @@ print(1 + 2 + 3)
 print(1 + 2 + 3)
 print("안녕하세요!")
 print("안녕하세요!")
-<<<<<<< HEAD
-=======
 
 print("hello universe!")
 print("hello universe!")
+print("hi universe!")
 print("hello universe!")
+print("hi universe!")
 print("hello universe!")
+print("hi universe!")
 print("hello universe!")
-print("hello universe!")
-print("hello universe!")
-print("hello universe!")
-print("hello universe!")
+print("hi universe!")
 
->>>>>>> main
 print("hello world!")
 print("hello world!")
 print("hello world!")
 print("hello world!")
-<<<<<<< HEAD
 print("hello")
 print("hello")
 print("반갑습니다!")
-=======
->>>>>>> main
 
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
