7f567d9d3beb2c2e87eb8c3cffbbe3c7bdf6c389
jmyoon <wjdals.yoon14@gmail.com>
2021-01-26 20:09:46 +0900

    squash conflict해결중입니다!

--- test.py
+++ test.py
@@ -3,18 +3,31 @@ print("안녕하세요!")
 print("hello universe!")
 print("반갑습니다!")
 
-print('commit2')
-print('commit333333333333333333333333')
+print("GOOOOOOOD!")
+print('ksdjafhjsdfhlsdkjhsdjklflhsdjfffff')
+print('skjdfhaskdjfhsdakjfhsakdjfhsdakjfhsadkjfh')
+print(333)
+print('yeonu')
+print("vim")
+print("rebase연습")
+print("rebase연습~~~세번째 커밋")
+print("squash연습!")
+print("rebase연습!")
 
-
-print("안녕하세요!")
-
-print("hello universe!")
 print("hello universe!")
 print("hi universe!")
 print("hello universe!")
 print("hi universe!")
+
+print("conflict해결중,,,jm")
 
 print("hello world!")
 print("반갑습니다!")
 
+print("hello tiger")
+print("hello lion!")
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
+print("hi universe!")
